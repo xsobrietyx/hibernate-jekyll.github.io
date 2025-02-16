bf3dfa53b7e6e28b5d4410b0ad2f180bc6a0c341
Vadym Serdiuk <xsobrietyx@gmail.com>
2025-02-16 02:14:16 -0500

    additional changes

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git_modules_file="$(cat <-EOF
+git_modules_file="$(cat <<-EOF
 [submodule "themes/8-bit-ascii-theme"]
 	path = themes/8-bit-ascii-theme
 	url  = https://github.com/xsobrietyx/8-bit-ascii-theme.git
@@ -8,4 +8,15 @@ git_modules_file="$(cat <-EOF
 EOF
 )"
 
+sed '$d' .gitignore
+
 echo $git_modules_file > .gitmodules
+
+git submodule update --remote
+
+echo "$(cat <<-EOF
+.gitmodules
+EOF
+)" >> .gitignore
+
+echo "Done!"
